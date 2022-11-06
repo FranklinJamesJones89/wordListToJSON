6b26054331e756f50ab4a01393470a8236f66682
FranklinJamesJones89 <fjones89@icloud.com>
2022-11-06 16:37:44 -0500

    Initial commit

---
+++ string_to_json.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import json
+from pathlib import Path
+
+# get file
+user_file = open(Path.home() / 'Code/Scripts/test_file.txt')
+
+# create empty dictionary for usernames and passwords
+credentials_dict = {'usernames':[], 'passwords':[]}
+
+# iterate through words in file and append them to credentials_dict
+for names in user_file:
+    credentials_dict['usernames'].append(names.strip())
+    credentials_dict['passwords'].append(names.strip())
+
+# convert credentials_object into JSON data
+credentials_object = json.dumps(credentials_dict, indent=2)
+
+# print credentials_object
+print(type(credentials_object))
