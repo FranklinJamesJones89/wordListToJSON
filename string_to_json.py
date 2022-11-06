a8fff07b07b934cae5fe58b65a1c4bec50980aae
FranklinJamesJones89 <fjones89@icloud.com>
2022-11-06 18:01:02 -0500

    Update

--- string_to_json.py
+++ string_to_json.py
@@ -3,16 +3,14 @@
 import json
 from pathlib import Path
 
-# get file
-user_file = open(Path.home() / 'Code/Scripts/test_file.txt')
-
 # create empty dictionary for usernames and passwords
 credentials_dict = {'usernames':[], 'passwords':[]}
 
 # iterate through words in file and append them to credentials_dict
-for names in user_file:
-    credentials_dict['usernames'].append(names.strip())
-    credentials_dict['passwords'].append(names.strip())
+with open(Path.home() / 'Code/Scripts/test_file.txt') as user_file:
+    for names in user_file:
+        credentials_dict['usernames'].append(names.strip())
+        credentials_dict['passwords'].append(names.strip())
 
 # convert credentials_object into JSON data
 credentials_object = json.dumps(credentials_dict, indent=2)
